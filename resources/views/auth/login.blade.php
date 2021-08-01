10bce12e23fa873077dee4ac2b12d3755f4506d1
ledangtuankhoi <ledangtuankhoi@gmail.com>
2021-08-02 02:22:37 +0700

    func login and template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,92 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <link rel="stylesheet" href="{{ asset('public/backend/assets_login/css/style.css') }}">
 
+</head>
+
+<body class="img js-fullheight"
+    style="background-image: url({{ asset('public/backend/assets_login/images/bg.jpg') }});">
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-5">
+                    <h2 class="heading-section">Login</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-4">
+                    <div class="login-wrap p-0">
+                        <h3 class="mb-4 text-center">Have an account?</h3>
+                        <form method="POST" class="signin-form" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-group">
+                                {{-- username --}}
+                                <input type="text" class="form-control  @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email"
+                                    placeholder="Username"  autofocus>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                            <div class="form-group">
+                                {{-- pass --}}
+                                <input id="password-field" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password" placeholder="Password" required>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+                                <span toggle="#password-field"
+                                    class="fa fa-fw fa-eye field-icon toggle-password"></span>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
+                            <div class="form-group">
+                                <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+                            </div>
+                            <div class="form-group d-md-flex">
+                                <div class="w-50">
+                                    <label class="checkbox-wrap checkbox-primary"> 
                                         {{ __('Remember Me') }}
+                                        <input type="checkbox" {{ old('remember') ? 'checked' : '' }} checked>
+                                        <span class="checkmark"></span>
                                     </label>
                                 </div>
+                                <div class="w-50 text-md-right">
+                                    <a href="#" style="color: #fff">Forgot Password</a>
+                                </div>
                             </div>
+                        </form>
+                        <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
+                        <div class="social d-flex text-center">
+                            <a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span>
+                                Facebook</a>
+                            <a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span>
+                                Twitter</a>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </section>
+
+    <script src="{{ asset('public/backend/assets_login/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('public/backend/assets_login/js/popper.js') }}"></script>
+    <script src="{{ asset('public/backend/assets_login/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('public/backend/assets_login/js/main.js') }}"></script>
+
+</body>
+
+</html>
--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,68 +1,93 @@
-<div class="sidebar" data-color="purple" data-background-color="white" data-image="{{asset('public/backend/assets/img/sidebar-1.jpg');}}">
+<div class="sidebar" data-color="purple" data-background-color="white"
+    data-image="{{ asset('public/backend/assets/img/sidebar-1.jpg') }}">
     <!--
       Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
       Tip 2: you can also add an image using data-image tag
     -->
     <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
-        Creative Tim
-      </a></div>
+            Creative Tim
+        </a></div>
     <div class="sidebar-wrapper">
-      <ul class="nav">
-        <li class="nav-item active  ">
-          <a class="nav-link" href="./dashboard.html">
-            <i class="material-icons">dashboard</i>
-            <p>Dashboard</p>
-          </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="./user.html">
-            <i class="material-icons">person</i>
-            <p>User Profile</p>
-          </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="./tables.html">
-            <i class="material-icons">content_paste</i>
-            <p>Table List</p>
-          </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="./typography.html">
-            <i class="material-icons">library_books</i>
-            <p>Typography</p>
-          </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="./icons.html">
-            <i class="material-icons">bubble_chart</i>
-            <p>Icons</p>
-          </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="./map.html">
-            <i class="material-icons">location_ons</i>
-            <p>Maps</p>
-          </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="./notifications.html">
-            <i class="material-icons">notifications</i>
-            <p>Notifications</p>
-          </a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link" href="./rtl.html">
-            <i class="material-icons">language</i>
-            <p>RTL Support</p>
-          </a>
-        </li>
-        <li class="nav-item active-pro ">
-          <a class="nav-link" href="./upgrade.html">
-            <i class="material-icons">unarchive</i>
-            <p>Upgrade to PRO</p>
-          </a>
-        </li>
-      </ul>
+        <ul class="nav">
+            <li class="nav-item active  ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">dashboard</i>
+                    <p>Dashboard </p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">person</i>
+                    <p>Banner Management</p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">content_paste</i>
+                    <p>Category Management</p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">library_books</i>
+                    <p>Product Management</p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">bubble_chart</i>
+                    <p>Cart</p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">location_ons</i>
+                    <p>Post Category</p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">notifications</i>
+                    <p>Post Tag</p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">language</i>
+                    <p>Post Management</p>
+                </a>
+            </li>
+            <li class="nav-item  ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">unarchive</i>
+                    <p>Review Management</p>
+                </a>
+            </li>
+            <li class="nav-item  ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">unarchive</i>
+                    <p>Coupon Management</p>
+                </a>
+            </li>
+            <li class="nav-item  ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">unarchive</i>
+                    <p>User Management</p>
+                </a>
+            </li>
+            <li class="nav-item  ">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">unarchive</i>
+                    <p>Review Management</p>
+                </a>
+            </li>
+            <li class="nav-item active-pro">
+                <a class="nav-link" href="#">
+                    <i class="material-icons">unarchive</i>
+                    <p>Setting</p>
+                </a>
+            </li>
+        </ul>
     </div>
-  </div>
+</div>
