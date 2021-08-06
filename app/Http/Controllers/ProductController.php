613b4c494219f9e744ab599b12b22801fef168b0
ledangtuankhoi <ledangtuankhoi@gmail.com>
2021-08-07 03:18:55 +0700

    factory produc seeder

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product = Product::orderBy('id','DESC')->get();
+        return view('backend.product.index',compact('product'));
+    }
+
+    
+    public function brandStatus(Request $request)
+    {
+        if ($request->mode == true) {
+            DB::table('product')->where('id', $request->id)->update(['status' => 'active']);
+        } else {
+            DB::table('product')->where('id', $request->id)->update(['status' => 'inactive']);
+        }
+        return response()->json(['msg' => 'Successfully update status Category ', 'status' => true]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/backend/products/index.blade.php
@@ -0,0 +1,189 @@
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap-toggle-master/css/bootstrap-toggle.min.css') }}">
+@endsection
+@extends('backend.layouts.master')
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    @include('backend.notification')
+                </div>
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header card-header-primary">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <div>
+                                        <h4 class="card-title ">Category Table 
+                                            <a class="btn btn-outline-success" href="{{route('brand.create')}}" role="button">
+                                                <span class="material-icons mx-1">
+                                                    add_circle_outline
+                                                    </span>
+                                                            Crete
+                                            </a>
+                                        </h4>
+                                        <p class="card-category"> Here is a subtitle for this table</p>
+                                    </div>
+                                </div>
+
+
+                                <div>
+                                    <p>Total anner:{{ App\Models\Brand::count() }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class=" text-primary">
+                                        <th>
+                                            ID
+                                        </th>
+                                        <th>
+                                            Title
+                                        </th>
+                                        <th>
+                                            Slug
+                                        </th>
+                                        <th>
+                                            Photo
+                                        </th> 
+                                        <th>
+                                            Status
+                                        </th>
+                                        <th>
+                                            Actions
+                                        </th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                1
+                                            </td>
+                                            <td>
+                                                Dakota Rice
+                                            </td>
+                                            <td>
+                                                Niger
+                                            </td>
+                                            <td>
+                                                Oud-Turnhout
+                                            </td>
+                                            <td class="text-primary">
+                                                $36,738
+                                            </td>
+                                            <td class="text-primary">
+                                                $36,738
+                                            </td>
+                                        </tr>
+                                        @foreach ($brands as $item)
+                                            <tr>
+                                                <td>
+                                                    {{ $loop->iteration }}
+                                                </td>
+                                                <td>
+                                                    {{ $item->title }}
+                                                </td>
+                                                <td>
+                                                    {{ $item->slug }}
+                                                </td>
+                                                <td class="m-auto">
+                                                    <img src="{{ $item->photo }}" alt="" height="60">
+                                                </td> 
+                                                <td>
+                                                    <input name="toggle" value="{{ $item->id }}" class="toggle-tow"
+                                                        type="checkbox" data-toggle="toggle" data-on="Active"
+                                                        data-off="InActive" data-onstyle="success" data-offstyle="danger"
+                                                        data-size="small"
+                                                        {{ $item->status == 'active' ? 'checked' : ' ' }}>
+
+                                                </td>
+                                                <td>
+                                                    <a href="{{ route('brand.edit', $item->id) }}" data-toggle="tooltip"
+                                                        title="Edit" class=" float-left btn btn-sm btn-outline-warning">
+                                                        <span class="material-icons">edit</span>
+                                                    </a>
+                                                    <form action="{{ route('brand.destroy', $item->id) }}" method="post">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <a href="" data-toggle="tooltip" title="delete"
+                                                            data-id="{{ $item->id }}"
+                                                            class="dltBtn btn btn-sm btn-outline-danger">
+                                                            <span class="material-icons">delete</span>
+                                                        </a>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+    @section('scripts')
+        <script src="{{ asset('vendor/bootstrap-toggle-master/js/bootstrap-toggle.min.js') }}"></script>
+        {{-- cdn show alert --}}
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+        <script>
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $('.dltBtn').click(function(e) {
+                var form = $(this).closest('form');
+                var dataID = $(this).data('id');
+                e.preventDefault();
+                console.log('asd');
+                swal({
+                        title: "Are you sure?",
+                        text: "Once deleted, you will not be able to recover this imaginary file!",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            swal("Poof! Your imaginary file has been deleted!", {
+                                icon: "success",
+                            });
+                            form.submit();
+                        } else {
+                            swal("Your imaginary file is safe!");
+                        }
+                    });
+            });
+        </script>
+
+        <script>
+            $('input[name=toggle]').change(function() {
+                var mode = $(this).prop('checked');
+                var id = $(this).val();
+                // alert(mode);
+                $.ajax({
+                    URL: "{{route('brand.status')}}",
+                    type: "POST",
+                    data: {
+                        _token: '{{csrf_token()}}',
+                        mode: mode,
+                        id: id,
+                    },
+                    success: function(response) {
+                        if (response.status) {
+                            alert(response.msg)
+                        } else {
+                            alert('try again')
+                        }
+                    }
+                })
+            });
+        </script>
+    @endsection
