32d1097e5a102e638f1fd0ea2db7228114683176
ledangtuankhoi <ledangtuankhoi@gmail.com>
2021-08-21 15:41:07 +0700

    fontend checkout1

--- app/Http/Controllers/Fontend/CheckoutController.php
+++ app/Http/Controllers/Fontend/CheckoutController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Fontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
 {
     public function checkout1(){
-        return view('fontend.pages.checkout.checkout1'); 
+
+        $user = Auth::user();
+        return view('fontend.pages.checkout.checkout1',compact('user')); 
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        'user_id','order_number','product_id','sub_total','total_amout','coupon','delivery_change','quantity','first_name','last_name','email','phone','country','address','city','state','note',
+        'sfirst_name','slast_name','semail','sphone','scountry','saddress','scity','sstate',
+    ];
+
 }
